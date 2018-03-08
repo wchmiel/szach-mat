50a833dc0a12aa804cc9fc6ef88260ec853fdafb
Wojciech <wojton@localhost.com>
2018-03-08 18:47:57 +0100

    added menu open animation.

--- client/src/app/core/menu/menu.component.ts
+++ client/src/app/core/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('hamburger') hamburger: ElementRef;
+  @ViewChild('menuContainer') menuContainer: ElementRef;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+  }
+
+  toggleMenu() {
+    const hamClassList = this.hamburger.nativeElement.classList.value.split(' '); // array of hamburger classess
+
+    // toggling hamburger button
+    if ((hamClassList.findIndex((elem) => elem === 'icon-bar-eff-end')) === -1) {
+      this.renderer.addClass(this.hamburger.nativeElement, 'icon-bar-eff-end');
+      this.renderer.addClass(this.menuContainer.nativeElement, 'sz-menu-active');
+    } else {
+      this.renderer.removeClass(this.hamburger.nativeElement, 'icon-bar-eff-end');
+      this.renderer.removeClass(this.menuContainer.nativeElement, 'sz-menu-active');
+    }
   }
 
 }
