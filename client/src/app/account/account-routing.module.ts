865d9f063f22fa0f71c76c2da96207eb1e6e6a52
Wojciech <wojton@localhost.com>
2018-03-11 23:59:23 +0100

    recreate account component with index & sidebar - styles for sidebar v1

--- client/src/app/account/account-routing.module.ts
+++ client/src/app/account/account-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AccountComponent } from './account.component';
 import { IndexComponent } from './index/index.component';
 
 // Routes declaration for Account Module
 const accountRoutes: Routes = [
-  { path: 'account', component: IndexComponent }
+  { path: 'account', component: AccountComponent, children: [
+    { path: '', component: IndexComponent }
+  ]}
 ];
 
 @NgModule({
--- client/src/app/account/account.module.ts
+++ client/src/app/account/account.module.ts
@@ -3,11 +3,15 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
 
+import { AccountComponent } from './account.component';
 import { IndexComponent } from './index/index.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    IndexComponent
+    AccountComponent,
+    IndexComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
