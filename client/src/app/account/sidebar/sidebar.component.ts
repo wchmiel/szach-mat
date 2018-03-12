17d3104c5f1992727a843ebb89f9338d7d559d5a
Wojciech <wojton@localhost.com>
2018-03-12 21:04:47 +0100

    added sidebar main content.

--- client/src/app/account/sidebar/sidebar.component.ts
+++ client/src/app/account/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  @ViewChild('sidebarCont') sidebarCont: ElementRef;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.renderer.addClass(this.sidebarCont.nativeElement, 'sz-sidebar-cont-open');
+  }
+
+  onTogggleSidebar() {
+    const sidebarContClassList = this.sidebarCont.nativeElement.classList.value.split(' '); // array of sidebar classess
+
+    // toggling sidebarCont
+    if ((sidebarContClassList.findIndex((elem) => elem === 'sz-sidebar-cont-open')) === -1) {
+      this.renderer.removeClass(this.sidebarCont.nativeElement, 'sz-sidebar-cont-close');
+      this.renderer.addClass(this.sidebarCont.nativeElement, 'sz-sidebar-cont-open');
+    } else {
+      this.renderer.removeClass(this.sidebarCont.nativeElement, 'sz-sidebar-cont-open');
+      this.renderer.addClass(this.sidebarCont.nativeElement, 'sz-sidebar-cont-close');
+    }
   }
 
 }
